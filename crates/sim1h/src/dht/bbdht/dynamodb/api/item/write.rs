464bd0460fa28e2b959bdf414c2969c609bacf6a
David Meister <thedavidmeister@gmail.com>
2019-10-02 01:06:25 +1000

    test for transact_write_items feature

--- crates/sim1h/src/dht/bbdht/dynamodb/api/item/write.rs
+++ crates/sim1h/src/dht/bbdht/dynamodb/api/item/write.rs
@@ -9,15 +9,10 @@ use crate::trace::LogContext;
 use holochain_persistence_api::cas::content::AddressableContent;
 use rusoto_dynamodb::DynamoDb;
 use rusoto_dynamodb::PutItemInput;
+use crate::dht::bbdht::dynamodb::api::item::Item;
 use std::collections::HashMap;
 
-pub fn ensure_content(
-    log_context: &LogContext,
-    client: &Client,
-    table_name: &TableName,
-    content: &dyn AddressableContent,
-) -> BbDhtResult<()> {
-    tracer(&log_context, "ensure_content");
+pub fn content_to_item(content: &dyn AddressableContent) -> Item {
     let mut item = HashMap::new();
     item.insert(
         String::from(ADDRESS_KEY),
@@ -27,10 +22,20 @@ pub fn ensure_content(
         String::from(CONTENT_KEY),
         string_attribute_value(&String::from(content.content())),
     );
+    item
+}
+
+pub fn ensure_content(
+    log_context: &LogContext,
+    client: &Client,
+    table_name: &TableName,
+    content: &dyn AddressableContent,
+) -> BbDhtResult<()> {
+    tracer(&log_context, "ensure_content");
 
     client
         .put_item(PutItemInput {
-            item: item,
+            item: content_to_item(content),
             table_name: table_name.to_string(),
             ..Default::default()
         })
@@ -47,7 +52,56 @@ pub mod tests {
     use crate::dht::bbdht::dynamodb::api::table::exist::table_exists;
     use crate::dht::bbdht::dynamodb::api::table::fixture::table_name_fresh;
     use crate::dht::bbdht::dynamodb::client::local::local_client;
+    use crate::dht::bbdht::dynamodb::api::item::write::content_to_item;
+    use rusoto_dynamodb::DynamoDb;
+    use rusoto_dynamodb::Put;
+    use rusoto_dynamodb::TransactWriteItemsInput;
+    use rusoto_dynamodb::TransactWriteItem;
     use crate::trace::tracer;
+
+    #[test]
+    /// older versions of dynamodb don't support transact writes
+    /// test that this version is supported
+    fn transact_write_item_test() {
+        let log_context = "transact_write_item_test";
+
+        tracer(&log_context, "fixtures");
+        let local_client = local_client();
+        let table_name = table_name_fresh();
+        let content_a = content_fresh();
+        let content_b = content_fresh();
+
+        // ensure cas
+        assert!(ensure_cas_table(&log_context, &local_client, &table_name).is_ok());
+
+        // cas exists
+        assert!(table_exists(&log_context, &local_client, &table_name)
+        .expect("could not check table exists"));
+
+        // transact
+        local_client.transact_write_items(TransactWriteItemsInput {
+            transact_items: vec![
+                TransactWriteItem {
+                    put: Some(Put {
+                        table_name: table_name.clone(),
+                        item: content_to_item(&content_a),
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                },
+                TransactWriteItem {
+                    put: Some(Put {
+                        table_name: table_name.clone(),
+                        item: content_to_item(&content_b),
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                }
+            ],
+            ..Default::default()
+        }).sync().expect("could not transact write items");
+
+    }
 
     #[test]
     fn ensure_content_test() {
